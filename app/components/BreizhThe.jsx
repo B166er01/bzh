06ce2e2ade6e530d2f13af77e9d172fab2e397f6
B166er01 <b166erpro@gmail.com>
2024-08-08 12:03:35 +0200

    3d init

--- app/components/BreizhThe.jsx
+++ app/components/BreizhThe.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { Environment, OrthographicCamera } from "@react-three/drei";
+import { Environment, Float, OrthographicCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import React, { Suspense } from "react";
 
@@ -13,7 +13,14 @@ const BreizhThe = () => {
         <Suspense>
           <Environment preset="sunset" />
           <OrthographicCamera makeDefault zoom={10} position={[0, 0, 50]} />
-          <BottleCoca />
+          <Float
+            speed={3} // Animation speed, defaults to 1
+            rotationIntensity={1} // XYZ rotation intensity, defaults to 1
+            floatIntensity={1} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
+            floatingRange={[0.2, 1]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
+          >
+            <BottleCoca />
+          </Float>
         </Suspense>
       </Canvas>
     </div>
--- app/components/Experience.jsx
+++ app/components/Experience.jsx
@@ -3,7 +3,7 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Can } from "./models/Can";
-import { Environment, OrthographicCamera } from "@react-three/drei";
+import { Environment, Float, OrthographicCamera } from "@react-three/drei";
 
 const ModelViewer = () => {
   return (
@@ -12,7 +12,14 @@ const ModelViewer = () => {
         <Suspense>
           <Environment preset="sunset" />
           <OrthographicCamera makeDefault zoom={10} position={[0, 0, 50]} />
-          <Can />
+          <Float
+            speed={3} // Animation speed, defaults to 1
+            rotationIntensity={1} // XYZ rotation intensity, defaults to 1
+            floatIntensity={1} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
+            floatingRange={[0.2, 1]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
+          >
+            <BottleCoca />
+          </Float>
         </Suspense>
       </Canvas>
     </div>
