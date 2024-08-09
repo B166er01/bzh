6704e328f6482a3ccd807da24d941aab57cf9ba8
B166er01 <b166erpro@gmail.com>
2024-08-09 09:22:27 +0200

    marquee

--- app/components/Marquee2.jsx
+++ app/components/Marquee2.jsx
@@ -9,27 +9,19 @@ const Marquee2 = () => {
   const secondText = useRef(null);
   const slider = useRef(null);
 
-  // const firstText2 = useRef(null);
-  // const secondText2 = useRef(null);
-  // const slider2 = useRef(null);
-
   let xPercent = 0;
-  // let xPercent2 = 0;
-
   let direction = -1;
+  let animationFrameId;
 
   useEffect(() => {
     gsap.set(secondText.current, {
       left: secondText.current.getBoundingClientRect().width,
     });
-    // gsap.set(secondText2.current, {
-    //   left: secondText2.current.getBoundingClientRect().width,
-    // });
 
-    requestAnimationFrame(animate);
+    animationFrameId = requestAnimationFrame(animate);
 
     return () => {
-      // Clean up any resources if needed
+      cancelAnimationFrame(animationFrameId);
     };
   }, []);
 
@@ -46,21 +38,10 @@ const Marquee2 = () => {
       },
     });
 
-    // gsap.to(slider2.current, {
-    //   scrollTrigger: {
-    //     trigger: document.documentElement,
-    //     scrub: 0.5,
-    //     start: "top bottom",
-    //     end: "bottom top",
-    //     pinSpacer: false,
-    //     onUpdate: (e) => (direction = e.direction * 1),
-    //   },
-    // });
-
-    requestAnimationFrame(animate);
+    animationFrameId = requestAnimationFrame(animate);
 
     return () => {
-      // Clean up any resources if needed
+      cancelAnimationFrame(animationFrameId);
     };
   }, []);
 
@@ -73,17 +54,8 @@ const Marquee2 = () => {
     gsap.set(firstText.current, { xPercent: xPercent });
     gsap.set(secondText.current, { xPercent: xPercent });
 
-    // if (xPercent2 < -100) {
-    //   xPercent2 = 0;
-    // } else if (xPercent2 > 0) {
-    //   xPercent2 = -100;
-    // }
-    // gsap.set(firstText2.current, { xPercent: xPercent2 });
-    // gsap.set(secondText2.current, { xPercent: xPercent2 });
-
-    requestAnimationFrame(animate);
     xPercent += 0.04 * direction;
-    // xPercent2 -= 0.04 * direction;
+    animationFrameId = requestAnimationFrame(animate);
   };
 
   return (
@@ -94,7 +66,6 @@ const Marquee2 = () => {
           className="relative whitespace-nowrap font-creamCake text-myRed"
         >
           <p className="relative pr-5 m-0 capitalize text-8xl" ref={firstText}>
-            {" "}
             breizh cola - breizh cola - breizh cola - breizh cola - breizh cola
             - breizh cola -
           </p>
@@ -102,28 +73,11 @@ const Marquee2 = () => {
             className="absolute top-0 left-[100%] m-0 text-8xl pr-5 capitalize"
             ref={secondText}
           >
-            {" "}
             breizh cola - breizh cola - breizh cola - breizh cola - breizh cola
             - breizh cola -
           </p>
         </div>
       </div>
-
-      {/* <div className="absolute z-10 -translate-x-3 top-1/2 -rotate-6 border-y-2 border-skin-border ">
-        <div ref={slider2} className="relative z-20 whitespace-nowrap ">
-          <p className="relative pr-5 m-0 uppercase text-7xl " ref={firstText2}>
-            {" "}
-            blackbird studio - blackbird studio - blackbird studio -
-          </p>
-          <p
-            className="absolute top-0 left-[100%] m-0 text-7xl pr-5 uppercase "
-            ref={secondText2}
-          >
-            {" "}
-            blackbird studio - blackbird studio - blackbird studio -
-          </p>
-        </div>
-      </div> */}
     </div>
   );
 };
